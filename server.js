27a33d9fbf9ac7977bef7f2f711c6acf068a3d28
Niels Koster <n.koster@portavita.eu>
2020-07-25 19:48:55 +0200

    handle websocket connections

--- server.js
+++ server.js
@@ -14,4 +14,12 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room })
 })
 
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        console.log(roomId, userId)
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+    })
+})
+
 server.listen(3333)
