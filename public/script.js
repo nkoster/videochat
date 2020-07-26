eeaa916eb7d9568fd407d47bd6fed864a208baeb
Niels Koster <n.koster@portavita.eu>
2020-07-26 04:22:37 +0200

    connect and close users

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ const myPeer = new Peer(undefined, {
 })
 const videoGrid = document.querySelector('#video-grid')
 const myVideo = document.createElement('video')
+const peers = {}
 
 myVideo.muted = true
 
@@ -13,15 +14,38 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream)
+    myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', remoteVideStream => {
+            addVideoStream(video, remoteVideStream)
+        })
+    })
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+})
+
+socket.on('user-disconnected', userId => {
+    console.log('DISCONNECT', userId)
+    peers[userId] && peers[userId].close()
 })
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
 })
 
-socket.on('user-connected', userId => {
-    console.log('CONNECTED', userId)
-})
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', remoteVideoStream => {
+        addVideoStream(video, remoteVideoStream)
+    })
+    call.on('close', _ => {
+        video.remove()
+    })
+    peers[userId] = call
+}
 
 function addVideoStream(video, stream) {
     video.srcObject = stream
