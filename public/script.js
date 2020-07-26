f3e5f7c90caa97f29775e3f3726c78cc06cae845
Niels Koster <n.koster@portavita.eu>
2020-07-26 03:36:11 +0200

    Create stream and load myVideo

--- public/script.js
+++ public/script.js
@@ -3,6 +3,17 @@ const myPeer = new Peer(undefined, {
     host: '/',
     port: '4444'
 })
+const videoGrid = document.querySelector('#video-grid')
+const myVideo = document.createElement('video')
+
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+})
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
@@ -11,3 +22,11 @@ myPeer.on('open', id => {
 socket.on('user-connected', userId => {
     console.log('CONNECTED', userId)
 })
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', _ => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
